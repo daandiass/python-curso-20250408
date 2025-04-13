f1fb32610e4a145c93e7dc230352abe570b93cc6
Danilo <104972340+daandiass@users.noreply.github.com>
2025-04-13 19:23:25 -0300

    Update 1-lista.py_Exercicio_4

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,24 @@
 # Crie uma lista apenas com elementos numéricos
-lista = [1,2,3,4,5,6]
+anos = [1987, 2017, 2012, 1974]
+
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-lista = [1,2,3,'r','Danilo','1992','1.68']
+pessoa = ['Crislaine', 36, 2010, ['python', 'r', 'javascript', 'ruby'], True, ['nadar', 'ler', 'pedalar'], False]
+
 # Imprima na tela apenas os 5 primeiros elementos da lista
-print (4)
+print(pessoa[:5])
+
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+elementos_indice_par = pessoa[::2]
+print(elementos_indice_par)
 
 # Remova da lista o último item
+pessoa.pop()
+print(pessoa)
 
 # Insira na lista um novo item
+pessoa.append('calopsita')
+print(pessoa)
 
 # Remova da lista um item específico
+pessoa.remove(2010)
+print(pessoa)

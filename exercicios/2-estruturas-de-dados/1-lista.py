06a8866a5c2840bc189387b39cc809f6de782d3d
Danilo <104972340+daandiass@users.noreply.github.com>
2025-04-13 19:20:13 -0300

    Update 1-lista.py_Exercicio_4

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,9 +1,9 @@
 # Crie uma lista apenas com elementos numéricos
-
+lista = [1,2,3,4,5,6]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista = [1,2,3,'r','Danilo','1992','1.68']
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print (4)
 # Crie um slice na lista para que imprima na tela os elementos de índice par
 
 # Remova da lista o último item
